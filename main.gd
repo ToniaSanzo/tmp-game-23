b9d8c77a03bbc3902d22eb02c37a26495e28e3a6
Tonia Sanzo <44346865+ToniaSanzo@users.noreply.github.com>
2025-11-08 21:25:17 -0500

    change music

--- main.gd
+++ main.gd
@@ -36,32 +36,32 @@ func new_game():
 
 
 func _on_mob_timer_timeout():
-	# Create a new instance of the Mob scene.
-	var mob = mob_scene.instantiate()
-
-	# Choose a random location on Path2D.
-	var mob_spawn_location = $MobPath/MobSpawnLocation
 	
-	mob_spawn_location.progress_ratio = randf()
+	for i in score / 15:
+		# Create a new instance of the Mob scene.
+		var mob = mob_scene.instantiate()
 
-	# Set the mob's position to the random location.
-	mob.position = mob_spawn_location.position
-	
-	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
+		# Choose a random location on Path2D.
+		var mob_spawn_location = $MobPath/MobSpawnLocation
+		
+		mob_spawn_location.progress_ratio = randf()
 
-	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	
-	
+		# Set the mob's position to the random location.
+		mob.position = mob_spawn_location.position
+		
+		# Set the mob's direction perpendicular to the path direction.
+		var direction = mob_spawn_location.rotation + PI / 2
 
-	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
-	#mob.AnimatedSprite2D.flip_h = mob.velocity.x >= 0
+		# Add some randomness to the direction.
+		direction += randf_range(-PI / 4, PI / 4)
 
-	# Spawn the mob by adding it to the Main scene.
-	add_child(mob)
+		# Choose the velocity for the mob.
+		var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+		mob.linear_velocity = velocity.rotated(direction)
+		#mob.AnimatedSprite2D.flip_h = mob.velocity.x >= 0
+
+		# Spawn the mob by adding it to the Main scene.
+		add_child(mob)
 
 
 func _on_score_timer_timeout():
